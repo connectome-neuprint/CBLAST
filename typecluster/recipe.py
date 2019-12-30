bd8b54c5faf825f340c2e6f197a29a75de05cae4
Stephen Plaza <plazas@janelia.hhmi.org>
2019-12-30 11:01:57 -0500

    add new recipe for iterative calling of connectivity features optionally initialized by a set of features

--- typecluster/recipe.py
+++ typecluster/recipe.py
@@ -113,4 +113,88 @@ def cblast_workflow_simple(npclient, dataset, neuronlist, est_neuron_per_cluster
 
     return features_type
 
+ def cblast_workflow(npclient, dataset, neuronlist, est_neuron_per_cluster,
+         prev_features=None, iterations=0,
+         use_saved_types=True, customtypes={}, postprocess=sigmoid_process(),
+        sort_types=True, minconn=3, roi_restriction=None):
+    """Wrapper for "features.compute_connections_similarity_features" that allow users to provide
+    initial features (instead of customtypes) and number of iterations.
+
+    Note: the cluster types produced by "prev_features" have higher priority than customtypes
+    which have higher priority to the types stored in neuprint already if enabled. 
+
+    Args:
+        npclient (object): neuprint client object
+        dataset (str): name of neuprint dataset
+        neuronlist (list): list of body ids
+        est_neuron_per_cluster (int): estimated number of neurons per cluster (set high?)
+        prev_features (dataframe): features to use for clustering if available
+        iterations (int): number of iterations to iteratively refine connectivity reesults
+        (default 0: automatically determine the number of iterations):
+        use_saved_types(boolean): use types stored in neuprint already to guide clustering
+        customtypes (dict or df): mapping of body ids to a cluster id (over-rules pre-exising types)
+        postprocess (func): set with *_process function (each function allows users to set input and output to 0)
+        (other paramters)
+        sort_types (boolean): True to sort, False to collapse
+        minconn (int): Only consider connections >= minconn
+        roi_restriction (list): ROIs to restrict feature extraction (default: no restriction)
+    Returns:
+        dataframe: index: body ids; columns: different features 
+    """
     
+    if customtypes is not None and type(customtypes) == pd.DataFrame:
+        customtypes =  dict(zip(customtypes["bodyid"], customtypes["type"]))
+
+    # update custom types with provided features
+    if prev_features is not None:
+        hcluster = cluster.hierarchical_cluster(features_pro)
+        cluster2neuron, neuron2cluster = hcluster.get_partitions(len(neuronlist)//est_neuron_per_cluster)
+        customtypes_new =  dict(zip(neuron2cluster["bodyid"], neuron2cluster["type"]))
+        customtypes.update(customtypes_new)
+   
+
+    features_type = None
+    lastdist = 9999999999
+
+    # get and save all ccnnectivity features
+    replay_data = None
+    replay_data = features.compute_connection_similarity_features(npclient, dataset, neuronlist,
+            use_saved_types=use_saved_types, custom_types=custom_types, postprocess=postprocess, 
+            sort_types=sort_types, minconnn=minconn, roi_restriction=roi_restriction, dump_replay=True)
+
+    # compute type features based on predicted clusters
+    num_iterations = 0
+    neuron2cluster = None
+    while True:
+        num_iterations += 1
+        customtypes = {}
+        if neuron2cluster is not None:
+            customtypes =  dict(zip(neuron2cluster["bodyid"], neuron2cluster["type"]))
+
+        # add new cell type groupings
+        p1, p2, p3, p4, old_body2type = replay_data
+        old_body2type.update(customtypes)
+        replay_data_mod = (p1,p2,p3,p4,old_body2type)
+
+        # compute type features based on predicted clusters
+        replay_data = features.compute_connection_similarity_features(npclient, dataset, neuronlist,
+                use_saved_types=use_saved_types, custom_types=custom_types, postprocess=postprocess, 
+                sort_types=sort_types, minconnn=minconn, roi_restriction=roi_restriction, replay_data=replay_data_mod)
+        # iteratively refine to specified limit or when no improvement found
+        if num_iterations == iterations:
+            break
+
+        hcluster = cluster.hierarchical_cluster(features_type)
+        cluster2neuron, neuron2cluster, dist, bpair  = hcluster.get_partitions(len(neuronlist)//est_neuron_per_cluster, return_max=True)
+        print(f"Computed type clusters: {dist}")
+
+        # keep iterating until distance gets larger or doesn't change much
+        diff = abs(dist - lastdist)
+        if dist >= lastdist or diff < 0.0001:
+            break
+        lastdist = dist
+
+    return features_type
+
+
+
