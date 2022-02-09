f54b868cbd85d9528a8c83f0c8bb292044462480
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2022-02-09 15:01:33 -0500

    replayed a few changes that wouldn't merge cleanly; also .gitignore PyCharm files

--- cblast/features.py
+++ cblast/features.py
@@ -516,7 +516,7 @@ def extract_projection_features(npclient, neuronlist,
 def compute_connection_similarity_features(npclient, neuronlist,
         use_saved_types=True, customtypes={}, postprocess=scaled_process(0.5, 0.5, [0.9,0.0,0.1]),
         sort_types=True, pattern_only=False, minconn=3, roi_restriction=None,
-        dump_replay=False, replay_data = None, morph_only=False, hack_test=False):
+        dump_replay=False, replay_data = None, morph_only=False, statuses=["Anchor"]):
     """Computes an pairwise adjacency matrix for the given set of neurons.
 
     This function looks at inputs and outputs for the set of neurons.  The connections
@@ -546,11 +546,9 @@ def compute_connection_similarity_features(npclient, neuronlist,
         dump_replay (boolean): dumps features after parsing neuprint
         replay_data (tuple): data to enable replay
         morph_only (boolean): EXPERIMENTAL FEATURE only uses the root morpho type for each prior type
+        statuses (list of strings): only consider target neurons with the given statuses; default "Anchor" only
     Returns:
         dataframe: index: body ids; columns: different features
-
-    Note: only connection to traced neurons are considered.
-
     """
 
     if pattern_only:
@@ -631,17 +629,6 @@ def compute_connection_similarity_features(npclient, neuronlist,
                     if row["body1"] not in io_list:
                         io_list[row["body1"]] = {}
 
-                    if hack_test:
-                        # only take top 5 plus buffer
-                        if len(io_list[row["body1"]]) >= 5:
-                            count_list = []
-                            for name, count in io_list[row["body1"]].items():
-                                count_list.append(count)
-                            count_list.sort()
-                            count_list.reverse()
-                            if (count_list[4] - (count_list[4]**(1/2))) > totconn:
-                                continue
-
                     if feat_type not in io_list[row["body1"]]:
                         io_list[row["body1"]][feat_type] = 0
                     io_list[row["body1"]][feat_type] += totconn
@@ -652,7 +639,7 @@ def compute_connection_similarity_features(npclient, neuronlist,
             outputsquery = f"""\
                 WITH {currlist} AS TARGETS
                 MATCH(n :Neuron)-[x :ConnectsTo]->(m :Neuron)
-                WHERE n.bodyId in TARGETS AND m.status="Traced" AND x.weight >= {minconn}
+                WHERE n.bodyId in TARGETS AND m.status IN {statuses} AND x.weight >= {minconn}
                 RETURN n.bodyId AS body1, x.roiInfo AS info, m.bodyId AS body2, m.type AS type, x.weight AS weight
                 ORDER BY weight DESC
             """
@@ -660,11 +647,10 @@ def compute_connection_similarity_features(npclient, neuronlist,
             inputsquery = f"""\
                 WITH {currlist} AS TARGETS
                 MATCH(n :Neuron)<-[x :ConnectsTo]-(m :Neuron)
-                WHERE n.bodyId in TARGETS AND m.status="Traced" AND x.weight >= {minconn}
+                WHERE n.bodyId in TARGETS AND m.status IN {statuses} AND x.weight >= {minconn}
                 RETURN n.bodyId AS body1, x.roiInfo AS info, m.bodyId AS body2, m.type AS type, x.weight AS weight
                 ORDER BY weight DESC
             """
-            print(f"fetch batch {iter1}")
 
             outputs_list = {}
             inputs_list = {}
